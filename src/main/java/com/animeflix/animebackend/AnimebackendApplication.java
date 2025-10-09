18d9b52af7180a5c5ecba649c18daff0b3ebf32c
ChimUng <duynguyen19087@gmail.com>
2025-10-09 10:06:35 +0700

    api service animebackend module base

---
+++ src/main/java/com/animeflix/animebackend/AnimebackendApplication.java
@@ -0,0 +1,13 @@
+package com.animeflix.animebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimebackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/animeflix/animebackend/AnimebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animeflix.animebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
