e106d5408ff963acbc08d18014a0d5ad53c24ebe
ChimUng <duynguyen19087@gmail.com>
2025-10-10 16:25:03 +0700

    test animetranslate done

--- animetranslate/src/main/java/com/animeflix/animetranslate/controller/TranslateTestController.java
+++ animetranslate/src/main/java/com/animeflix/animetranslate/controller/TranslateTestController.java
@@ -1,7 +1,9 @@
 package com.animeflix.animetranslate.controller;
 
+import com.animeflix.animetranslate.model.TranslationRequest;
 import com.animeflix.animetranslate.model.TranslationResponse;
 import com.animeflix.animetranslate.service.GeminiClient;
+import com.animeflix.animetranslate.service.PromptBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +17,19 @@ import java.util.Map;
 public class TranslateTestController {
 
     private final GeminiClient geminiClient;
+    private final PromptBuilder promptBuilder;
 
-    public TranslateTestController(GeminiClient geminiClient) {
+    public TranslateTestController(GeminiClient geminiClient, PromptBuilder promptBuilder) {
         this.geminiClient = geminiClient;
+        this.promptBuilder = promptBuilder;
     }
 
     @GetMapping
     public Mono<List<TranslationResponse>> testTranslate() {
-        List<Map<String, String>> mockPrompts = List.of(
-                Map.of("anilistId", "1", "prompt", "Title: One Piece\nDescription: The story of a boy who wants to become the Pirate King.")
+        List<TranslationRequest> mockRequests = List.of(
+                new TranslationRequest(1, "One Piece", "The story of a boy who wants to become the Pirate King.")
         );
-        return geminiClient.translate(mockPrompts);
+        List<Map<String, String>> prompts = promptBuilder.build(mockRequests);
+        return geminiClient.translate(prompts);
     }
 }
