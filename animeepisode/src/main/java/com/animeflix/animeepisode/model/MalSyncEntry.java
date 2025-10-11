0d8f7a337c9954d94acde92392f811fe3ff2659a
ChimUng <duynguyen19087@gmail.com>
2025-10-11 16:46:34 +0700

    add Mal model

---
+++ animeepisode/src/main/java/com/animeflix/animeepisode/model/MalSyncEntry.java
@@ -0,0 +1,10 @@
+package com.animeflix.animeepisode.model;
+
+import lombok.Data;
+
+@Data
+public class MalSyncEntry {
+    private String providerId;
+    private String sub;
+    private String dub;
+}
