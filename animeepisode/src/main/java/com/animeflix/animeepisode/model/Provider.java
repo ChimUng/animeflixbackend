fef78e76e186d50358fa7dd2eea1e934de8324ff
ChimUng <duynguyen19087@gmail.com>
2025-10-11 16:47:01 +0700

    add Provider model

---
+++ animeepisode/src/main/java/com/animeflix/animeepisode/model/Provider.java
@@ -0,0 +1,18 @@
+package com.animeflix.animeepisode.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Provider {
+    private String providerId;
+    private String id;
+    private Boolean consumet;
+    private Map<String, List<Episode>> episodes;  // "sub" -> List<Episode>, "dub" -> List<Episode>
+}
