2e29f8977cc53bd9304447f67c0b8cd698e9a5a0
ChimUng <duynguyen19087@gmail.com>
2025-10-11 16:48:10 +0700

    add basic logic

---
+++ animeepisode/src/main/java/com/animeflix/animeepisode/AnimeepisodeApplication.java
@@ -0,0 +1,13 @@
+package com.animeflix.animeepisode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeepisodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeepisodeApplication.class, args);
+    }
+
+}
---
+++ animeepisode/src/test/java/com/animeflix/animeepisode/AnimeepisodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animeflix.animeepisode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeepisodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
