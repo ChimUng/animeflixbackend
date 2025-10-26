a52f5d61d3bc7e14ac8eb59c45c1097fa9b2ed85
ChimUng <duynguyen19087@gmail.com>
2025-10-26 16:18:42 +0700

    add External providers api

---
+++ animestream/src/main/java/com/animeflix/animestream/config/ExternalApiConfig.java
@@ -0,0 +1,83 @@
+package com.animeflix.animestream.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ExternalApiConfig {
+
+    @Value("${consumet.uri}")
+    private String consumetUri;
+
+    @Value("${malsync.uri}")
+    private String malsyncUri;
+
+    @Value("${zoro.uri}")
+    private String zoroUri;
+
+    @Value("${gogo.uri}")
+    private String gogoUri;
+
+    @Value("${anify.url}")
+    private String anifyUrl;
+
+    @Value("${animepahe.url}")
+    private String animepaheUrl;
+
+    @Value("${animapping.url}")
+    private String animappingUrl;
+
+    @Value("${anify.schedule.url}")
+    private String anifyScheduleUrl;
+
+    private ExchangeStrategies exchangeStrategies() {
+        return ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
+                .build();
+    }
+
+
+    @Bean
+    public WebClient consumetWebClient() {
+        return WebClient.builder().baseUrl(consumetUri).build();
+    }
+
+    @Bean
+    public WebClient malsyncWebClient() {
+        return WebClient.builder().baseUrl(malsyncUri).exchangeStrategies(exchangeStrategies()).build();
+    }
+
+    @Bean
+    public WebClient zoroWebClient() {
+        return WebClient.builder().baseUrl(zoroUri).build();
+    }
+
+    @Bean
+    public WebClient gogoWebClient() {
+        return WebClient.builder().baseUrl(gogoUri).build();
+    }
+
+
+    @Bean
+    public WebClient anifyWebClient() {
+        return WebClient.builder().baseUrl(anifyUrl).exchangeStrategies(exchangeStrategies()).build();
+    }
+
+    @Bean
+    public WebClient animepaheWebClient() {
+        return WebClient.builder().baseUrl(animepaheUrl.replace("{anime_session}/{session}", "")).build();  // Adjust dynamic
+    }
+
+    @Bean
+    public WebClient animappingWebClient() {
+        return WebClient.builder().baseUrl(animappingUrl.split("\\?")[0]).exchangeStrategies(exchangeStrategies()).build();  // Base without query
+    }
+
+    @Bean
+    public WebClient anifyScheduleWebClient() {
+        return WebClient.builder().baseUrl(anifyScheduleUrl).build();
+    }
+}
