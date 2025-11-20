3470c131134e8f13654f1f87b1c9d127f4896a09
ChimUng <duynguyen19087@gmail.com>
2025-11-20 22:03:43 +0700

    push developer controller using apikey

---
+++ auth-service/src/main/java/com/animeflix/authservice/controller/DeveloperController.java
@@ -0,0 +1,25 @@
+package com.animeflix.authservice.controller;
+
+import com.animeflix.authservice.DTO.RegisterDevRequest;
+import com.animeflix.authservice.DTO.RegisterDevResponse;
+import com.animeflix.authservice.service.DeveloperService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class DeveloperController {
+
+    private final DeveloperService authService;
+
+    @PostMapping("/dev/register")
+    public Mono<ResponseEntity<RegisterDevResponse>> registerDev(
+            @RequestBody RegisterDevRequest req) {
+        return authService.registerDeveloper(req)
+                .map(resp -> ResponseEntity.status(HttpStatus.CREATED).body(resp));
+    }
+}
