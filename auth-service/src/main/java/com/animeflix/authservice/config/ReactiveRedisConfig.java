5c024daaf63891ef6335fc0dcbed78dcab6000c1
ChimUng <duynguyen19087@gmail.com>
2025-11-22 17:19:37 +0700

    add count rarelimit for apikey using

---
+++ auth-service/src/main/java/com/animeflix/authservice/config/ReactiveRedisConfig.java
@@ -0,0 +1,27 @@
+package com.animeflix.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class ReactiveRedisConfig {
+
+    @Bean
+    @Primary
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        StringRedisSerializer valueSerializer = new StringRedisSerializer();
+
+        RedisSerializationContext<String, String> context = RedisSerializationContext
+                .<String, String>newSerializationContext(keySerializer)
+                .value(valueSerializer)
+                .build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+}
