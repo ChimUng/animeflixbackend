f1ed23ee286793fdf708b1b819e3bc48a876f0b5
ChimUng <duynguyen19087@gmail.com>
2025-12-15 18:17:30 +0700

    remove animerecomendation

--- animerecommendation/src/main/java/com/animeflix/animerecommendation/model/ScoredRecentEpisode.java
+++
@@ -1,9 +0,0 @@
-package com.animeflix.animerecommendation.model;
-
-public class ScoredRecentEpisode extends RecentEpisode {
-    private Integer score;
-
-    // Getter and setter
-    public Integer getScore() { return score; }
-    public void setScore(Integer score) { this.score = score; }
-}
