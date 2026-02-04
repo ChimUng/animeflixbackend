68805ff058c50aab1715bdb7262ad916087fc9ae
ChimUng <duynguyen19087@gmail.com>
2026-02-04 20:14:59 +0700

    delete dupcated

--- animestream/src/main/java/com/animeflix/animestream/exception/CacheException.java
+++
@@ -1,6 +0,0 @@
-package com.animeflix.animestream.exception;
-
-public class CacheException extends RuntimeException {
-    public CacheException(String message) { super(message); }
-    public CacheException(String message, Throwable cause) { super(message, cause); }
-}
