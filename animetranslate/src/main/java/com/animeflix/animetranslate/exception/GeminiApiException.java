a4edf39e7f0fb98b77c32b1c33b12b687999a4a5
ChimUng <duynguyen19087@gmail.com>
2026-05-19 09:39:01 +0700

    Delete animetranslate directory

--- animetranslate/src/main/java/com/animeflix/animetranslate/exception/GeminiApiException.java
+++
@@ -1,7 +0,0 @@
-package com.animeflix.animetranslate.exception;
-
-public class GeminiApiException extends TranslationException {
-    public GeminiApiException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
